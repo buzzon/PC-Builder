31d6e760c4b079c983959c7bf3ce6c3cad2de7ad
buzzon <buzzondev@gmail.com>
2020-10-13 14:42:21 +0500

    Create models

--- pcb/pcbcore/admin.py
+++ pcb/pcbcore/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(CPU)
+admin.site.register(GPU)
+admin.site.register(RAM)
+admin.site.register(MotherBoard)
+admin.site.register(ROM)
+admin.site.register(PowerSupply)
+
+admin.site.register(Manufacturer)
+admin.site.register(Socket)
+admin.site.register(Chipset)
--- pcb/pcbcore/models.py
+++ pcb/pcbcore/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    title = models.CharField(max_length=256, unique=True)
+
+
+class Socket(models.Model):
+    title = models.CharField(max_length=256, unique=True)
+
+
+class Chipset(models.Model):
+    title = models.CharField(max_length=256, unique=True)
+
+
+class CPU(models.Model):
+    title = models.CharField(max_length=256)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    cores = models.IntegerField()
+    threads = models.IntegerField()
+    frequency = models.FloatField()
+    socket = models.ForeignKey(Socket, on_delete=models.CASCADE)
+    value = models.IntegerField()
+    price = models.IntegerField()
+
+
+class GPU(models.Model):
+    title = models.CharField(max_length=256)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    memory = models.IntegerField()
+    value = models.IntegerField()
+    price = models.IntegerField()
+
+
+class RAM(models.Model):
+    title = models.CharField(max_length=256)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    frequency = models.FloatField()
+    size = models.IntegerField()
+    value = models.IntegerField()
+    price = models.IntegerField()
+
+
+class MotherBoard(models.Model):
+    title = models.CharField(max_length=256)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    chipset = models.ForeignKey(Chipset, on_delete=models.CASCADE)
+    socket = models.ForeignKey(Socket, on_delete=models.CASCADE)
+    value = models.IntegerField()
+    price = models.IntegerField()
+
+
+class ROM(models.Model):
+    TYPE = (
+        ('H', 'HDD'),
+        ('S', 'SSD'),
+    )
+
+    title = models.CharField(max_length=256)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    type = models.CharField(max_length=1, choices=TYPE, default='H')
+    read = models.IntegerField()
+    write = models.IntegerField()
+    size = models.IntegerField()
+    value = models.IntegerField()
+    price = models.IntegerField()
+
+
+class PowerSupply(models.Model):
+    title = models.CharField(max_length=256)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    power = models.IntegerField()
+    value = models.IntegerField()
+    price = models.IntegerField()
+
+
