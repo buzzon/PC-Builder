0b71d1e277ed194abdd7d9f6c28b59cbb61775f1
buzzon <buzzondev@gmail.com>
2020-11-13 02:02:45 +0500

    connect postgresql

--- pcb/settings/settings.py
+++ pcb/settings/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'pcb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': params.get('db', 'name'),
+        'USER': params.get('db', 'user'),
+        'PASSWORD': params.get('db', 'password'),
+        'HOST': params.get('db', 'host'),
+        'PORT': params.get('db', 'port'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
